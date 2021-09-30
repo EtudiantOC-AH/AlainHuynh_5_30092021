29d9a07266410681975be42907b5814c3b973870
Alain Huynh <alain.huynh86@gmail.com>
2021-09-30 19:44:50 +0200

    call api index

---
+++ front/scripts/index.js
@@ -0,0 +1,22 @@
+fetch('http://localhost:3000/api/products')
+    .then((response) => response.json())
+    .then(products => {
+        console.log(products);
+        let productsHTML = ""
+        products.forEach(product => {
+            productsHTML += `
+            <a href="./product.html?${product._id}">
+            <article>
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+            <h3 class="productName"> ${product.name}</h3>
+            <p class="productDescription">${product.description}</p>
+            </article>
+          </a>
+        `
+        })
+        document.getElementById('items').innerHTML = productsHTML
+    })
+    .catch((error) => {
+        let messageError = document.querySelector(".items");
+        messageError.innerHTML = "Une erreur est survenue...";
+    })
